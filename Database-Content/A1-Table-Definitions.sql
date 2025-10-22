0ea8c2dbac301074c8c739d46bd83d45d68b4e7c
Adi Pleyer <adi.pleyer@sap.com>
2025-10-22 18:13:07 +0200

    Update file naming scheme

--- Database-Content/A1-Table-Definitions.sql
+++ Database-Content/A1-Table-Definitions.sql
@@ -1,3 +1,15 @@
+--- delete all tables for Hotel Booking System ---
+--- handle with care ...
+
+DROP TABLE BigAirports;
+DROP TABLE Airports;
+DROP TABLE Countries;
+DROP TABLE Reservations;
+DROP TABLE Guests;
+DROP TABLE Booking;
+DROP TABLE Rooms;
+DROP TABLE Hotels;
+
 --- table definitions for Hotel Booking System ---
 --- drops are in a separate file ---
 
--- Lecture-Files/1-5-z-excercise-sitze.sql
+++ Lecture-Files/1-5-z-excercise-sitze.sql
@@ -52,9 +52,6 @@ select block, avg(SitzPreis)
 						;
 
 
----
-
-
 
 --- random free seat in room '01' with price category <= 5 - is this correct ???
 select * from SITZ, (select round(random()*4)+1 as r1, round(random()*4)+1 as r2) as r
